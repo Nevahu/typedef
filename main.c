2fdb0b6b0100f04e2981fb50f22b050ecff88d66
Nevahu <nevahuarslan26@gmail.com>
2022-08-28 23:58:24 +0300

    typedef

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+// typedef veri tiplerine istedigim ismi vermemi saglar.
+//kullanım sekli typedef eski_veri_tipi yeni_veri_tipi seklindeki gibidir.
+int main()
+{
+    //mesela
+    typedef int nevahu;
+    // artik int degil de nevahu ismini kullanabilirim.
+}
